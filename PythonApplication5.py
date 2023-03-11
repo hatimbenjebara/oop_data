155909636288da594a0b1dbfa86dacae33d7f95a
Hatim Benjebara <108321950+hatimbenjebara@users.noreply.github.com>
2023-03-11 11:19:03 +0100

    Add files via upload

--- PythonApplication5.py
+++ PythonApplication5.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-class DataFrameWrapper:
+class DataFrameregular:
     def __init__(self, data):
         self.df = pd.DataFrame(data)
     
@@ -22,22 +22,30 @@ class DataFrameWrapper:
         self.df.drop_duplicates(inplace = True)# remove the duplicate rows permanently and inplace = True to make a changes
     def null_data(self):
         #check if there s any null values in our data
+        a1=self.df.shape
         print(self.df.isnull())# to show where null value is present if is null then true if not then its false 
         print(self.df.isnull().sum()) #show the count of null values in each column
+        a2=self.df.shape
+        print(a2)
+        if a1==a2 :
+            print("there's no duplicate in data")
+        else:
+            print("we had a duplicate and we had removed it ")
+    def select_columns(self,name1,name2,col1):
+        print(self.df[self.df[name1].isin([name2])])
+        a = self.df[self.df[name1].isin([name2])]
     def add_column(self, name, values):
         self.df[name] = values
-    
     def drop_column(self, name):
-        self.df.drop(columns=[name], inplace=True)
-    
+        self.df.drop(columns=[name], inplace=True)  
     def filter_rows(self, condition):
         self.df = self.df[condition]
 data = pd.read_csv('Uber.csv')
-wrapper = DataFrameWrapper(data)
-print(wrapper.info_data())
-print(wrapper.duplicate_data())
-print(wrapper.null_data())
-wrapper.drop_column('STOP*')
-print(wrapper.get_dataframe())
-wrapper.filter_rows(wrapper.get_dataframe()['MILES*'] != '5.1')
-print(wrapper.get_dataframe())
+regular = DataFrameregular(data)
+print(regular.info_data())
+print(regular.duplicate_data())
+print(regular.null_data())
+regular.drop_column('STOP*')
+print(regular.get_dataframe())
+regular.filter_rows(regular.get_dataframe()['MILES*'] != '5.1')
+print(regular.get_dataframe())
